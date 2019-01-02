ac30ec4afcbfe9cb6310c35264c6cd50972c38e6
Joseph Jung <joseph.jung@icloud.com>
2019-01-02 06:49:24 -0700

    Initial Commit

---
+++ todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  todoey
+//
+//  Created by Joseph Jung on 1/2/19.
+//  Copyright © 2019 Joseph Jung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
