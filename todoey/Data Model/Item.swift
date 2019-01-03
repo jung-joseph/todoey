581b623434e281f626db53e402f2793789a6355f
Joseph Jung <joseph.jung@icloud.com>
2019-01-02 17:05:57 -0700

    convert to MVC design pattern

---
+++ todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  todoey
+//
+//  Created by Joseph Jung on 1/2/19.
+//  Copyright © 2019 Joseph Jung. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
